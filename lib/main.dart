2291cd97ffec32b4fac191a7d9402991863dc93a
Kuwait-zero <hussein-_2000@hotmail.com>
2024-10-23 08:51:36 -0400

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,17 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:mini_project/pages/home_page.dart';
-import 'package:mini_project/pages/sign_in.dart';
-import 'package:mini_project/pages/sign_up.dart';
-import 'package:mini_project/providers/auth_providers.dart';
 import 'package:provider/provider.dart';
 
 void main() {
   runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider<AuthProvider>(create: (_) => AuthProvider()),
-      ],
-      child: MyApp(),
-    ),
+    // MultiProvider(
+    //   providers: [
+    //     ChangeNotifierProvider<PetsProvider>(create: (_) => PetsProvider()),
+    //     ChangeNotifierProvider<AuthProvider>(create: (_) => AuthProvider()),
+    //   ],
+    // child:
+    MyApp(),
+    // ),
   );
 }
 
@@ -21,37 +19,37 @@ class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: _router,
-    );
+    return MaterialApp(
+        // routerConfig: _router,
+        );
   }
 
-  final _router = GoRouter(
-    routes: [
-      GoRoute(
-        path: '/',
-        builder: (context, state) => const HomePage(),
-      ),
-      // GoRoute(
-      //   path: '/add',
-      //   builder: (context, state) => AddPage(),
-      // ),
-      GoRoute(
-        path: '/signup',
-        builder: (context, state) => SignupPage(),
-      ),
-      GoRoute(
-        path: '/signin',
-        builder: (context, state) => SigninPage(),
-      ),
-      // GoRoute(
-      //   path: '/update/:petId',
-      //   builder: (context, state) {
-      //     final pet = Provider.of<postProvider>(context).pets.firstWhere(
-      //         (pet) => pet.id.toString() == (state.pathParameters['petId']!));
-      //     return UpdatePage(pet: pet);
-      //   },
-      // ),
-    ],
-  );
+  // final _router = GoRouter(
+  //   routes: [
+  //     GoRoute(
+  //       path: '/',
+  //       builder: (context, state) => const HomePage(),
+  //     ),
+  //     GoRoute(
+  //       path: '/add',
+  //       builder: (context, state) => AddPage(),
+  //     ),
+  //     GoRoute(
+  //       path: '/update/:petId',
+  //       builder: (context, state) {
+  //         final pet = Provider.of<PetsProvider>(context).pets.firstWhere(
+  //             (pet) => pet.id.toString() == (state.pathParameters['petId']!));
+  //         return UpdatePage(pet: pet);
+  //       },
+  //     ),
+  //     GoRoute(
+  //       path: '/signup',
+  //       builder: (context, state) => SignupPage(),
+  //     ),
+  //     GoRoute(
+  //       path: '/signin',
+  //       builder: (context, state) => SigninPage(),
+  //     ),
+  // ],
+  // );
 }
