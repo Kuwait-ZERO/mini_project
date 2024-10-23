92446f93af3ae3581077262ae372438b590ee9ac
Kuwait-zero <hussein-_2000@hotmail.com>
2024-10-23 08:44:05 -0400

    client

---
+++ lib/models/user.dart
@@ -0,0 +1,55 @@
+import 'dart:js_interop';
+
+import 'package:json_annotation/json_annotation.dart';
+// part 'user.g.dart';
+
+@JsonSerializable()
+class User {
+  int? id;
+  String username;
+  String? password;
+
+  User({
+    this.id,
+    required this.username,
+    this.password,
+  });
+
+  User.fromJson(dynamic json)
+      : username = json["username"],
+        password = json["password"],
+        id = json["id"];
+
+  Map<String, dynamic> toJson() {
+    return {"username": username, "password": password, "id": id};
+  }
+
+  // factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+  // Map<String, dynamic> toJson() => _$UserToJson(this);
+}
+
+// void parsingExample() {
+//   Map json1 = {"username": "ghanim", "password": "123", "id": 1};
+//   Map json2 = {"username": "ghanim", "password": "123", "id": 1};
+//   Map json3 = {"username": "ghanim", "password": "123", "id": 1};
+
+//   var json = [
+//     {"username": "ghanim", "password": "123", "id": 1},
+//     {"username": "ghanim", "password": "123", "id": 2},
+//     {"username": "ghanim", "password": "123", "id": 3},
+//   ];
+
+  //  print(json["id"]);
+
+  // User user = User.a(json);
+
+  // List<User> users = [];
+
+  // for (int i = 0; json.length < i; i++) {
+  //   var u = User.fromJson(json[i]);
+  //   users.add(u);
+  // }
+
+  // var users2 = json.map((j) => User.fromJson2(j)).toList();
+
+// }
---
+++ lib/services/auth_service.dart
@@ -0,0 +1,33 @@
+import 'package:adopt_app/models/user.dart';
+import 'package:adopt_app/services/client.dart';
+import 'package:dio/dio.dart';
+import 'package:mini_project/services/Client.dart';
+
+class AuthServices {
+  final Dio _dio = Dio();
+
+  final _baseUrl = 'https://coded-pets-api-auth.eapi.joincoded.com';
+
+  Future<String> signup({required User user}) async {
+    late String token;
+    try {
+      Response response =
+          await _dio.post(_baseUrl + '/signup', data: user.toJson());
+      token = response.data["token"];
+    } on DioError catch (error) {
+      print(error);
+    }
+    return token;
+  }
+
+  Future<String> signin({required User user}) async {
+    late String token;
+    try {
+      Response response = await Client.dio.post('/signin', data: user.toJson());
+      token = response.data["token"];
+    } on DioError catch (error) {
+      print(error);
+    }
+    return token;
+  }
+}
